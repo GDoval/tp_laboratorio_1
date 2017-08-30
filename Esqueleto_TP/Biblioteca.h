44e51ac6a438576a0a7f81fd2ebea99c33d4f5e4
GDoval <charly_sxxi@hotmail.com>
2017-08-30 17:51:34 -0300

    Calculadora terminada, falta documentar las funciones. Biblioteca .h y .c

---
+++ Esqueleto_TP/Biblioteca.h
@@ -0,0 +1,8 @@
+float IngresoFloat(char texto[]);
+float Suma(float n1, float n2);
+float Resta (float n1, float n2);
+float Multi(float n1, float n2);
+float Divi(float n1, float n2);
+char NoEsCero(float numero);
+long long int Factorial (int num);
+char MayorCero (int numero);
--- Esqueleto_TP/main.c
+++ Esqueleto_TP/main.c
@@ -1,20 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
-//#include "funciones.h"
-float IngresoFloat(char texto[]);
-float Suma(float n1, float n2);
-float Resta (float n1, float n2);
-float Multi(float n1, float n2);
-float Divi(float n1, float n2);
-char Validacion(float numero);
+#include "Biblioteca.h"
 int main()
 {
     char seguir='s';
-    int opcion=0;
+    int opcion=0, flagDiv = 0, flagFac = 0;
     float num1 = 0;
     float num2 = 0;
     float sum, res, mul, div;
-    char noEsCero;
+    char noEsCero, mayorQueCero;
+    long long int facto;
+
 
     while(seguir =='s')
     {
@@ -25,7 +21,7 @@ int main()
         printf("5- Calcular la division (A/B)\n");
         printf("6- Calcular la multiplicacion (A*B)\n");
         printf("7- Calcular el factorial (A!)\n");
-        printf("8- Calcular todas las operacione\n");
+        printf("8- Calcular todas las operaciones\n");
         printf("9- Salir\n");
 
         scanf("%d",&opcion);
@@ -33,45 +29,121 @@ int main()
         switch(opcion)
         {
             case 1:
-                num1 = IngresoFloat("Ingrese el operando A: ");
                 system("clear");
+                num1 = IngresoFloat("Ingrese el operando A: ");
+                //system("clear");
                 break;
             case 2:
-                num2 = IngresoFloat("Ingrese operando B: ");
                 system("clear");
+                num2 = IngresoFloat("Ingrese operando B: ");
+                //system("clear");
                 break;
             case 3:
                 sum = Suma(num1,num2);
+                system("clear");
                 printf("\nLa suma de los operandos es: %.3f\n", sum);
                 printf("\n\n");
                 break;
             case 4:
                 res = Resta(num1,num2);
+                system("clear");
                 printf("\nLa resta de los operandos es: %.3f\n", res);
                 printf("\n\n");
                 break;
             case 5:
-                noEsCero = Validacion(num2);
+                noEsCero = NoEsCero(num2);
                 if ( noEsCero == 'n' )
                 {
+                    system("clear");
                     printf("\nNo se puede dividir por cero, papafrita!\n");
                     printf("\n\n");
 
                 }else
                 {
                     div = Divi(num1,num2);
+                    system("clear");
                     printf("\nLa division de los operandos es: %.3f\n", div);
                     printf("\n\n");
                 }
                 break;
             case 6:
                 mul = Multi(num1,num2);
+                system("clear");
                 printf("\nLa multiplicacion de los operandos es: %.3f\n", mul);
                 printf("\n\n");
                 break;
             case 7:
+                mayorQueCero = MayorCero(num1);
+                if (mayorQueCero == 's')
+                {
+                    facto = Factorial(num1);
+                    system("clear");
+                    printf("\nEl factorial de %.0f es %lli \n", num1, facto);
+                    printf("\n\n");
+                }else
+                {
+                    system("clear");
+                    printf("\nNo puede hacerse el factorial de un numero negativo. \n");
+                    printf("\n\n");
+                }
+
                 break;
             case 8:
+                sum = Suma(num1,num2);
+                res = Resta(num1,num2);
+                mul = Multi(num1,num2);
+                noEsCero = NoEsCero(num2);
+                mayorQueCero = MayorCero(num1);
+                if ( noEsCero == 'n' )
+                {
+                    flagDiv = 1;
+
+                }else
+                {
+                    div = Divi(num1,num2);
+                    flagDiv = 0;
+
+                }
+                if (mayorQueCero == 's')
+                {
+                    facto = Factorial(num1);
+                    flagFac = 0;
+
+                }else
+                {
+                    flagFac = 1;
+                }
+                if (flagDiv == 1 && flagFac == 1)
+                {
+                    system("clear");
+                    printf("\nLa suma de los operandos es: %.3f\nSu resta es: %.3f\nSu multiplicacion es: %.3f\nNo puede dividirse por cero\nNo puede realizarse el factorial de un numero negativo\n", sum, res, mul);
+                    printf("\n\n");
+                }else
+                {
+                    if (flagDiv == 0 && flagFac == 1)
+                    {
+                        system("clear");
+                        printf("\nLa suma de los operandos es: %.3f\nSu resta es: %.3f\nSu multiplicacion es: %.3f\nSu division es: %.3f \nNo puede realizarse el factorial de un numero negativo\n", sum, res, mul, div);
+                        printf("\n\n");
+                    }else
+                    {
+                        if (flagDiv == 1 && flagFac == 0)
+                        {
+                            system("clear");
+                            printf("\nLa suma de los operandos es: %.3f\nSu resta es: %.3f\nSu multiplicacion es: %.3f\nNo puede dividirse por cero\nEl factorial del operando A es: %lli\n", sum, res, mul, facto);
+                            printf("\n\n");
+                        }else
+                        {
+                            if (flagDiv == 0 && flagFac == 0)
+                            {
+                                system("clear");
+                                printf("\nLa suma de los operandos es: %.3f\nSu resta es: %.3f\nSu multiplicacion es: %.3f\nSu division es: %.3f \nEl factorial del operando A es: %lli\n", sum, res, mul, div, facto);
+                                printf("\n\n");
+                            }
+                        }
+                    }
+                }
+
                 break;
             case 9:
                 seguir = 'n';
@@ -85,48 +157,3 @@ int main()
 }
 
 
-float IngresoFloat(char texto[]) // La funcion recive como parametro un texto que indica el tipo de dato que quiere ingresarse. Imprime por pantalla el texto ingresado como parametro, y toma y retorna un numero de tipo float.
-{
-    float numero;
-    printf("%s", texto);
-    scanf("%f", &numero);
-    return numero;
-}
-
-float Resta (float n1, float n2) // Se restan los numeros pasados como argumentos.
-{
-    float resta;
-    resta = n1 - n2;
-    return resta;
-}
-
-float Suma(float n1, float n2) // Se suman los numeros pasados como argumentos.
-{
-    float suma;
-    suma = n1 + n2;
-    return suma;
-}
-
-float Multi(float n1, float n2) //Se multiplican los numeros pasados como argumentos.
-{
-    float multi;
-    multi = n1 * n2;
-    return multi;
-}
-
-float Divi(float n1, float n2) //Se dividen los numeros pasados como argumentos. La validacion de si el divisor es un 0 se realiza en una funcion aparte.
-{
-    float divi;
-    divi = n1 / n2;
-    return divi;
-}
-
-char Validacion(float numero) // Si el numero ingresado como parametro es igual a 0, retorna 'n'.
-{
-    char resultado;
-    if (numero == 0)
-    {
-        resultado = 'n';
-    }
-    return resultado;
-}
